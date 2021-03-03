db64ec908642af906a9688f31e0fe8790927b9e9
Dzmitry Hlushak <hlushak.dzmitry@gmail.com>
2021-03-03 16:45:08 +0300

    feat: add close btn for Color component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,26 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      colors: []
+      colors: [
+        {
+          "id": "43435-4654654-654-tt-dg-4-3543-65-46-54243-r",
+          "title": "ocean at dusk",
+          "color": "#00c4e2",
+          "rating": "5"
+        },
+        {
+          "id": "6465-446-65765764-tt-dg-4-3543-65-46-54243-r",
+          "title": "lawn",
+          "color": "#26ac56",
+          "rating": "3"
+        },
+        {
+          "id": "43435-4654654-654-tt-dg-4-3543-66-54243-454",
+          "title": "bright red",
+          "color": "#ff0000",
+          "rating": "0"
+        }
+      ]
     }
 
     this.addColor = this.addColor.bind(this);
--- src/components/ColorList/Color/Color.jsx
+++ src/components/ColorList/Color/Color.jsx
@@ -4,12 +4,13 @@ import { StarRating } from '../../StarRating/StarRating.jsx';
 import './Color.scss';
 
 
-const Color = ({title, color, rating = 0}) =>
+const Color = ({title, color, rating = 0, onRemove=f=>f, onRate=f=>f}) =>
   <section className="color">
     <h1 className="color__title">{title}</h1>
+    <button onClick={onRemove} className="color__remove">x</button>
     <div className="color-elem" style={{backgroundColor: color}}></div>
     <div>
-      <StarRating starsSelected={rating}/>
+      <StarRating starsSelected={rating} onRate={onRate}/>
     </div>
   </section>;
 
