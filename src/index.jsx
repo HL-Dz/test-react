9a7c23e0e86ba191097793cbb30c058e31253923
Dzmitry Hlushak <hlushak.dzmitry@gmail.com>
2021-03-14 21:45:00 +0300

    feat: create base store

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+import {store} from './redux/store.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "./scss/main.scss";
---
+++ src/redux/store.js
@@ -0,0 +1,30 @@
+const constants = {
+  ADD_COLOR: 'ADD_COLOR',
+  SORT_COLOR: 'SORT_COLOR',
+  RATE_COLOR: 'RATE_COLOR',
+  REMOVE_COLOR: 'REMOVE_COLOR'
+}
+
+
+export let store = {
+  colors: [
+    {
+      "id": "43435-4654654-654-tt-dg-4-3543-65-46-54243-r",
+      "title": "ocean at dusk",
+      "color": "#00c4e2",
+      "rating": "5"
+    },
+    {
+      "id": "6465-446-65765764-tt-dg-4-3543-65-46-54243-r",
+      "title": "lawn",
+      "color": "#26ac56",
+      "rating": "3"
+    },
+    {
+      "id": "43435-4654654-654-tt-dg-4-3543-66-54243-454",
+      "title": "bright red",
+      "color": "#ff0000",
+      "rating": "0"
+    }
+  ]
+};
