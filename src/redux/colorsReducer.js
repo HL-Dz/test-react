7066a038a927ac01d11206fe5c943409571f3ba9
Dzmitry Hlushak <hlushak.dzmitry@gmail.com>
2021-03-17 20:17:52 +0300

    refactor: change colorsReducer

--- src/redux/colorsReducer.js
+++ src/redux/colorsReducer.js
@@ -3,26 +3,28 @@ const RATE_COLOR = 'RATE_COLOR';
 const REMOVE_COLOR = 'REMOVE_COLOR';
 
 
-let initialState = [
-  {
-    "id": "43435-4654654-654-tt-dg-4-3543-65-46-54243-r",
-    "title": "ocean at dusk",
-    "color": "#00c4e2",
-    "rating": "5"
-  },
-  {
-    "id": "6465-446-65765764-tt-dg-4-3543-65-46-54243-r",
-    "title": "lawn",
-    "color": "#26ac56",
-    "rating": "3"
-  },
-  {
-    "id": "43435-4654654-654-tt-dg-4-3543-66-54243-454",
-    "title": "bright red",
-    "color": "#ff0000",
-    "rating": "0"
-  }   
-];
+let initialState = {
+  colors: [
+    {
+      id: "43435-4654654-654-tt-dg-4-3543-65-46-54243-r",
+      title: "ocean at dusk",
+      color: "#00c4e2",
+      rating: 5
+    },
+    {
+      id: "6465-446-65765764-tt-dg-4-3543-65-46-54243-r",
+      title: "lawn",
+      color: "#26ac56",
+      rating: 3
+    },
+    {
+      id: "43435-4654654-654-tt-dg-4-3543-66-54243-454",
+      title: "bright red",
+      color: "#ff0000",
+      rating: 0
+    }
+  ]
+};
 
 const colorReducer = (state={}, action) => {
   switch(action.type) {
@@ -48,14 +50,20 @@ const colorReducer = (state={}, action) => {
 const colorsReducer = (state = initialState, action) => {
   switch(action.type) {
     case ADD_COLOR:
-      return [
+      return {
         ...state,
-        colorReducer({}, action)
-      ]
+        colors: [...state.colors, colorReducer({}, action)]
+      }
     case RATE_COLOR:
-      return state.map(color => colorReducer(color, action))
+      return {
+        ...state,
+        colors: state.colors.map(color => colorReducer(color, action))
+      }
     case REMOVE_COLOR:
-      return state.filter(color => color.id !== action.id)
+      return {
+        ...state,
+        colors: state.colors.filter(color => color.id !== action.id)
+      }
     default:
       return state
   }
