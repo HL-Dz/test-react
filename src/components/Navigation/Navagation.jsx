9437f58b8ae458478a683a2c459e102dae20ad92
Dzmitry Hlushak <hlushak.dzmitry@gmail.com>
2021-03-23 14:30:05 +0300

    refactor: change base navigation and add active style

--- src/components/Navigation/Navagation.jsx
+++ src/components/Navigation/Navagation.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Navigation.scss';
+
+const selectedStyle = {
+  backgroundColor: '#fff',
+  color: 'rgb(26, 26, 39)',
+  fontWeight: 'bold'
+}
+
 
 
 const Navigation = () =>
   <div className="navigation">
     <nav className="nav">
-      <Link to="/" className="nav__link">Home</Link>
-      <Link to="about" className="nav__link">About</Link>
-      <Link to="events" className="nav__link">Events</Link>
-      <Link to="products" className="nav__link">Products</Link>
-      <Link to="contact" className="nav__link">Contact</Link>
+      <NavLink to="/" className="nav__link">Home</NavLink>
+      <NavLink to="about" className="nav__link" activeStyle={selectedStyle}>About</NavLink>
+      <NavLink to="events" className="nav__link" activeStyle={selectedStyle}>Events</NavLink>
+      <NavLink to="products" className="nav__link" activeStyle={selectedStyle}>Products</NavLink>
+      <NavLink to="contact" className="nav__link" activeStyle={selectedStyle}>Contact</NavLink>
     </nav>
   </div>
 
